b1b6ef94d69229b0a1a7da9a4343a3894bfa2815
brettlangdon <brett@blangdon.com>
2016-08-09 13:56:57 -0400

    update output formatting

--- needler/results.py
+++ needler/results.py
@@ -1,27 +1,105 @@
+from collections import OrderedDict
+import json
+import os
+import sys
+import traceback
 import unittest
+
+TERMINAL_WIDTH = 80
+if 'COLUMNS' in os.environ:
+    TERMINAL_WIDTH = int(os.environ['COLUMNS']) - 1
 
 
 class NeedlerResults(unittest.TestResult):
-    @classmethod
-    def from_config(cls, config):
-        return cls()
+    def __init__(self, config):
+        super(NeedlerResults, self).__init__()
+        self.config = config
+        self.tests = OrderedDict()
 
     def addError(self, test, error):
         super(NeedlerResults, self).addError(test, error)
-        print error
+        self.tests[test.name]['error'] = error
+        self.tests[test.name]['result'] = 'error'
 
     def addFailure(self, test, error):
         super(NeedlerResults, self).addFailure(test, error)
-        print error
+        self.tests[test.name]['error'] = error
+        self.tests[test.name]['result'] = 'failure'
 
     def addSuccess(self, test):
         super(NeedlerResults, self).addSuccess(test)
-        print 'SUCCESS'
+        self.tests[test.name]['result'] = 'success'
 
     def startTest(self, test):
         super(NeedlerResults, self).startTest(test)
-        print 'Running test case: {name}'.format(name=test.name)
+        self.tests[test.name] = OrderedDict(
+            result='running',
+            name=test.name,
+            url=test.url,
+            selector=test.selector,
+            width=test.width,
+            height=test.height,
+            error=None,
+            baseline_file=self.config.get_baseline_file(test.name),
+            output_file=self.config.get_output_file(test.name),
+            diff_file=self.config.get_diff_file(test.name),
+        )
+        self.print_status()
+
+    def stopTest(self, test):
+        super(NeedlerResults, self).stopTest(test)
+        self.print_status()
+
+    def stopTestRun(self):
+        self.print_status()
+        sys.stderr.write('\n')
+        sys.stderr.flush()
+
+    def print_status(self):
+        line = '. '
+        for test in self.tests.values():
+            marker = '.'
+            if test['result'] == 'error':
+                marker = 'E'
+            elif test['result'] == 'failure':
+                marker = 'F'
+            elif test['result'] == 'running':
+                marker = 'R'
+            line += marker
+        sys.stderr.write(line + '\r')
+        sys.stderr.flush()
 
     def print_results(self):
-        print ('Finished running tests: ran({ran}) errors({errors}) failures({failures})'
-               .format(ran=self.testsRun, errors=len(self.errors), failures=len(self.failures)))
+        if self.config.as_json:
+            json.dump(self.tests, sys.stdout)
+            sys.stdout.write('\n')
+        else:
+            for test_name, test in self.tests.items():
+                # Skip successful tests
+                if test['result'] == 'success':
+                    continue
+
+                sys.stdout.write('=' * TERMINAL_WIDTH + '\n')
+                sys.stdout.write('{result} {test_name}\n'.format(result=test['result'].upper(), test_name=test_name))
+                sys.stdout.write('=' * TERMINAL_WIDTH + '\n')
+                for key, value in test.items():
+                    if key == 'error':
+                        continue
+                    sys.stdout.write('{key}: {value}\n'.format(key=key, value=value))
+                sys.stdout.write('-' * TERMINAL_WIDTH + '\n')
+                _, message, tb = test['error']
+                sys.stdout.write('{message}\n'.format(message=message))
+                sys.stdout.write('-' * TERMINAL_WIDTH + '\n')
+                traceback.print_tb(tb, file=sys.stdout)
+                sys.stdout.flush()
+
+            total = len(self.tests)
+            success = len([t for t in self.tests.values() if t['result'] == 'success'])
+            error = len([t for t in self.tests.values() if t['result'] == 'error'])
+            failure = len([t for t in self.tests.values() if t['result'] == 'failure'])
+
+            sys.stdout.write('=' * TERMINAL_WIDTH + '\n')
+            sys.stdout.write('Results: total({total}) success({success}) failure({failure}) error({error})\n'
+                             .format(total=total, success=success, failure=failure, error=error))
+            sys.stdout.write('=' * TERMINAL_WIDTH + '\n')
+        sys.stdout.flush()
